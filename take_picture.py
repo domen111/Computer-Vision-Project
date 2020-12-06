5d873d219aeb5c5936c37224e3401de744981c06
domen111 <domen111@gmail.com>
2020-12-07 00:20:40 +0800

    take_picture.py

--- take_picture.py
+++ take_picture.py
@@ -1,19 +1,24 @@
 import cv2 as cv
 import time
 import argparse
+import pathlib
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--dir', help='images dir')
+parser.add_argument('-d', '--dir', help='images directory')
+parser.add_argument('-no', '--no', help='the filename index of the first image', type=int, default=1)
 args = parser.parse_args()
 
-cameras_no = [0, 1]
+if args.dir:
+    pathlib.Path(args.dir).mkdir(parents=True, exist_ok=True)
+
+cameras_no = [1, 0]
 caps = [cv.VideoCapture(i) for i in cameras_no]
 for cap in caps:
     cap.set(cv.CAP_PROP_AUTO_EXPOSURE, 0)
     cap.set(cv.CAP_PROP_EXPOSURE, 0)
     # cap.set(3,160)
     # cap.set(4,120)
-no = 1
+no = args.no
 while True:
     succ = True
     for cap in caps:
@@ -28,8 +33,11 @@ while True:
     key = cv.waitKey(100)
     if key == ord('s'):
         print(f'save img #{no}')
-        cv.imwrite(f'{args.dir}/l{no}.png', imgs[0])
-        cv.imwrite(f'{args.dir}/r{no}.png', imgs[1])
+        if not args.dir:
+            print('`--dir` missing')
+        else:
+            cv.imwrite(f'{args.dir}/img{no:04d}l.png', imgs[0])
+            cv.imwrite(f'{args.dir}/img{no:04d}r.png', imgs[1])
         no += 1
     elif key == ord('q'):
         break
